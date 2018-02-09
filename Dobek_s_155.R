e80e6d59fffffe9fc2f517f6e47f0db0419397bb
zjanna <zjoanna@up.poznan.pl>
2018-02-09 10:59:56 +0100

    Add files via upload

---
+++ Dobek_s_155.R
@@ -0,0 +1,47 @@
+(Dobek, Szwaczkowski 2007, s. 155) 
+
+Sprawdzamy, czy zależności między procentową zawartością łuski a masą tysiąca nasion (g) dwóch odmian słonecznika (oznaczonych odpowiednio jako A i B) są takie same. Badaniami objęto 30 roślin z każdej odmiany. Otrzymane wyniki zestawiono w Tablicy \@ref(dobek155).
+
+\begin{table}[H]
+\centering
+\caption{Dane - Dobek, Szwaczkowski (2007, s. 155)}
+\label{dobek155}
+\scalebox{0.9}{
+  \begin{tabular}{cccc}
+  \hline
+  \multicolumn{2}{c}{Odmiana A} & \multicolumn{2}{c}{Odmiana B} \\ \hline
+  \begin{tabular}[c]{@{}c@{}}procent \\ łuski\end{tabular} & \begin{tabular}[c]{@{}c@{}}masa tysiaca \\ nasion\end{tabular} & \begin{tabular}[c]{@{}c@{}}procent \\ łuski\end{tabular} & \begin{tabular}[c]{@{}c@{}}masa tysiaca \\ nasion\end{tabular} \\ \hline
+  0.24 & 55.37 & 0.49 & 50 \\
+  0.16 & 55.6 & 0.29 & 48 \\
+  0.22 & 52.61 & 0.46 & 52 \\
+  0.14 & 56.25 & 0.24 & 49 \\
+  0.26 & 61.26 & 0.26 & 51 \\
+  0.31 & 40.23 & 0.3 & 51 \\
+  0.19 & 58.46 & 0.46 & 48.26 \\
+  0.39 & 57.72 & 0.4 & 46.28 \\
+  0.20 & 49.76 & 0.36 & 47.58 \\
+  0.19 & 53.78 & 0.35 & 46.03 \\
+  0.3 & 52.97 & 0.36 & 46 \\
+  0.24 & 55.06 & 0.45 & 47.83 \\
+  0.36 & 65.57 & 0.46 & 53.53 \\
+  0.16 & 59.35 & 0.47 & 51.16 \\
+  0.01 & 56.88 & 0.39 & 48.98 \\
+  0.38 & 63.14 & 0.2 & 44.65 \\
+  0.37 & 54.49 & 0.23 & 45.13 \\
+  0.24 & 56.12 & 0.43 & 47.67 \\
+  0.09 & 58.37 & 0.48 & 53.29 \\
+  0.14 & 52.37 & 0.5 & 55.49 \\
+  0.42 & 50.03 & 0.5 & 55.1 \\
+  0.23 & 60.63 & 0.44 & 49.09 \\
+  0.24 & 53.62 & 0.24 & 47.13 \\
+  0.32 & 50.65 & 0.49 & 45.63 \\
+  0.36 & 54.9 & 0.43 & 47.71 \\
+  0.31 & 54.74 & 0.46 & 45.43 \\
+  0.12 & 54.65 & 0.48 & 49.94 \\
+  0.3 & 60.12 & 0.31 & 54.28 \\
+  0.02 & 50.34 & 0.52 & 47.24 \\
+  0.39 & 52.37 & 0.39 & 50.65 \\ \hline
+  \end{tabular}
+}
+\end{table}
+Należy dokonać weryfikacji hipotezy o równości współczynników korelacji dla obydwu odmian.
---
+++ kody.R
@@ -0,0 +1,25 @@
+\vspace{0.8cm}
+**Kod w R**
+```{r eval=FALSE,echo=TRUE,highlight=FALSE}
+
+```
+
+\vspace{0.8cm}
+**Realizacja w R**
+```{r plott13, eval=FALSE,echo=TRUE,fig.cap='plot13', fig.align='center',fig.pos='H',out.width='70%'}
+
+```
+
+**Realizacja w R**
+```{r tidy.opts=list(width.cutoff=37),comment=NA,prompt=TRUE}
+
+```
+
+#ustawienia globalne
+opts_chunk$set(fig.path='figure/', fig.align='center', fig.show='asis', fig.pos='ht!', dev.args=list(encoding='CP1250'))
+
+
+\vspace{0.8cm}
+**Podstawowe operacje na wektorach: **
+  
+  \texttt{
